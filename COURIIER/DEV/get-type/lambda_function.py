26e99bc5e87d4093972bd374c5d9f67b64cda396
Ithy <schreys.ithyvan@gmail.com>
2017-06-13 11:51:08 +0200

    Add courrier

---
+++ COURIIER/DEV/get-type/lambda_function.py
@@ -0,0 +1,11 @@
+def getType(event, context):
+    true=True
+    false=False
+    data={
+      "type": "pickup",
+      "postal": false,
+      "pickup": true,
+      "dropoff": false,
+      "linehaul": false
+    }
+    return data
